65256f519c8e3fc1b32cb7f3df0e771d951e605b
Nicolas Blanc <nicolasblanc360@gmail.com>
2017-06-18 16:16:27 +0200

    Quadratic Regression finished, need to correct prob.log2, in plot(X,..) -> plot(X[, 1:2])

--- script_TP4_nico.R
+++ script_TP4_nico.R
@@ -25,7 +25,16 @@ prob.ad(param, X, z, c(0.2,0.4,0.6,0.8))
 
 #----Logistic regression----
 #----------------------------
+
+#Linear logistic regression
 res <- separ1(X,z)
 param <- log.app(X, z, 1, 0.00001)
 
 prob.log(param$beta, X, z, c(0.2,0.4,0.6,0.8))
+
+#Quadratic logistic regression
+#Our individuals are defined in X = {X^1, X^2} 
+#First we have to define the new set of values part of X^2 = {X^1, X^2, X^1X^2, (X^1)^2, (X^2)^2} 
+Xquad <- cbind(X, X[,1]*X[,2], (X[,1])^2, (X[,2])^2)
+param <- log.app(Xquad, z, 1, 0.00001)
+prob.log2(param$beta, Xquad, z, c(0.2,0.4,0.6,0.8))
